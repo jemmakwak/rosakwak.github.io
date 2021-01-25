df888c2ae1dd15d95b7f233fc6ccf7a7228a6738
Jemma Kwak <yeogyoungkwak@berkeley.edu>
2021-01-24 19:26:15 -0800

    Add files via upload

--- assets/js/waypoints.js
+++ assets/js/waypoints.js
@@ -1,1 +1,36 @@
-$(document).ready(function(){var behind=$('.tagline').waypoint(function(direction){$(this.element).addClass('fadeIn-delay');this.destroy()},{offset:'50%'});var description=$('.intro').waypoint(function(direction){$(this.element).addClass('fadeIn-slower');this.destroy()},{offset:'50%'});new ClipboardJS('.email');$('.email').mouseenter(function(){$('#hover').show()});$('.email').mouseleave(function(){$('#hover').hide()});$('.email').click(function(){document.getElementById("hover").innerHTML="Copied! &#128588;";$('#hover').delay(800).fadeOut()})})
+$(document).ready(function() {
+
+    $('.transition').mouseenter(function() {
+        $('#transition').delay(800).fadeIn()
+    });
+    
+    var behind = $('.tagline').waypoint(function(direction) {
+        $(this.element).addClass('fadeIn-delay');
+        this.destroy()
+    }, {
+        offset: '50%'
+    });
+    var description = $('.intro').waypoint(function(direction) {
+        $(this.element).addClass('fadeIn-slower');
+        this.destroy()
+    }, {
+        offset: '50%'
+    });
+    var description = $('body').waypoint(function(direction) {
+        $(this.element).addClass('fadeIn-slower');
+        this.destroy()
+    }, {
+        offset: '50%'
+    });
+    new ClipboardJS('.email');
+    $('.email').mouseenter(function() {
+        $('#hover').show()
+    });
+    $('.email').mouseleave(function() {
+        $('#hover').hide()
+    });
+    $('.email').click(function() {
+        document.getElementById("hover").innerHTML = "Copied! &#128588;";
+        $('#hover').delay(800).fadeOut()
+    })
+})
